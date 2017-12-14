2e1a16897bda792b9aa711f2b7be34288b793b82
sanisunny <31963944+sanisunny@users.noreply.github.com>
2017-12-14 09:59:21 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+//Starter project help from W3 schools
+
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'userexampleemail@gmail.com',
+    pass: 'userexamplepassword'
+  }
+});
+
+var mailOptions = {
+  from: 'userexampleemail@gmail.com',
+  to: 'userexamplefriendemail@yahoo.com',
+  subject: 'Look at this cool email that I sent using Node.js!',
+  text: 'Who knew the things you could do with code!'
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+});
