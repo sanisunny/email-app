07dc4c1a5cd1bc0c36425d1bbeef4811d88f4517
sanisunny <31963944+sanisunny@users.noreply.github.com>
2017-12-18 10:13:22 -0600

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ var mailOptions = {
 
 transporter.sendMail(mailOptions, function(error, info){
   if (error) {
-    console.log(error);
+    throw(error);
   } else {
     console.log('Email sent: ' + info.response);
   }
